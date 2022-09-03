b15e3423f16675b2f5d347c6248407798655724d
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-02 22:07:38 -0300

    Musicas

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Para você é só um rolê, só mais uma ressaca");
+Console.WriteLine("Para ela outra mágoa, um gatilho, outro trauma");
+Console.WriteLine("Enquanto finaliza a saideira");
+Console.WriteLine("Destrói os sonhos de uma vida inteira");
+Console.WriteLine("Você curtindo o auge dos seus 30 e poucos anos");
+Console.WriteLine("E ela te esperando acordada, fazendo planos");
+Console.WriteLine("Você não vai ganhar nada com isso");
+Console.WriteLine("Não tem ninguém 'tá achando isso bonito");
+Console.WriteLine("Cadê sua responsabilidade?");
+Console.WriteLine("No seu lugar eu teria vergonha");
+Console.WriteLine("Por mim ela teria te deixado");
+Console.WriteLine("Fazer o que se ela te ama");
+Console.WriteLine("Então aproveita que ela nem sonha quem você é");
+
+
+
+
+
